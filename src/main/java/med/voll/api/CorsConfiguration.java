6fc794efd69f9958cda521bbd1b1bda1120bd917
Veronica <almeida.vrc@gmail.com>
2023-07-31 09:53:26 -0300

    receive data from API medico, add MedicoController (DTO)

---
+++ src/main/java/med/voll/api/CorsConfiguration.java
@@ -0,0 +1,4 @@
+package med.voll.api;
+
+public interface CorsConfiguration {
+}
---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,16 @@
+package med.voll.api.controller;
+
+import med.voll.api.medico.DadosCadastroMedico;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+    @RequestMapping
+    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+        System.out.println(dados);
+    }
+}
