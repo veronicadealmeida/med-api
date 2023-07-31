df4ae2b42d426821082589a56bcf25a9d79b4b99
Veronica <almeida.vrc@gmail.com>
2023-07-31 11:15:46 -0300

    migrations with Flyway

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,10 @@
 package med.voll.api.controller;
 
-import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +14,7 @@ public class MedicoController {
     @Autowired
     private MedicoRepository repository;
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
         repository.save(new Medico(dados));
     }
