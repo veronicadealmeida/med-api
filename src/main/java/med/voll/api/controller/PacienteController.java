7d5b459e28d487f245006fce0da3e3697f520151
Veronica <almeida.vrc@gmail.com>
2023-08-04 09:44:49 -0300

    Add pacakge domain

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,7 +2,10 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.paciente.*;
+import med.voll.api.domain.paciente.DadosListagemPaciente;
+import med.voll.api.domain.paciente.Paciente;
+import med.voll.api.domain.paciente.PacienteRepository;
+import med.voll.api.domain.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
--- src/main/java/med/voll/api/domain/paciente/DadosdetalhamentoPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DadosdetalhamentoPaciente.java
@@ -1,7 +1,6 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.paciente;
 
-import med.voll.api.endereco.Endereco;
-import med.voll.api.medico.Especialidade;
+import med.voll.api.domain.endereco.Endereco;
 
 public record DadosdetalhamentoPaciente(Long id, String nome, String email, String cpf, String telefone, Endereco endereco) {
 
