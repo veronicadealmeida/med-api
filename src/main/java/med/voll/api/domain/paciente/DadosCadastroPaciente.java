b10c075515cdc3218d21d3e2b7da3f474d706264
Veronica <almeida.vrc@gmail.com>
2023-08-04 10:32:49 -0300

    add 400 error handling and message validantion

--- src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
@@ -8,10 +8,17 @@ import jakarta.validation.constraints.Pattern;
 import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroPaciente(
-        @NotBlank String nome,
-        @NotBlank @Email String email,
-        @NotBlank String telefone,
-        @NotBlank @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}") String cpf,
-        @NotNull @Valid DadosEndereco endereco
+        @NotBlank(message = "{nome.obrigatorio}")
+        String nome,
+        @NotBlank(message = "{email.obrigatorio}")
+        @Email(message = "{email.invalido}")
+        String email,
+        @NotBlank(message = "{telefone.obrigatorio}")
+        String telefone,
+        @NotBlank(message = "{cpf.obrigatorio}")
+        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}", message = "{cpf.invalido}")
+        String cpf,
+        @NotNull @Valid
+        DadosEndereco endereco
 ) {
 }
--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,7 +11,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class TratadorDeErros {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity tratarErro() {
+    public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception) {
+        var erros = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
+
 }
