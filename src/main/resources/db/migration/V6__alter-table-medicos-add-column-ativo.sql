0492d7d6ab10c0fc893b34364ef5d3a613da055e
Veronica <almeida.vrc@gmail.com>
2023-08-10 15:02:23 -0300

    ajustes para banco postgress

--- src/main/resources/db/migration/V6__alter-table-medicos-add-column-ativo.sql
+++ src/main/resources/db/migration/V6__alter-table-medicos-add-column-ativo.sql
@@ -1,3 +1,3 @@
-alter table medicos add ativo smallint;
-update medicos set ativo =1;
+alter table medicos add ativo boolean default true;
+update medicos set ativo = true;
 alter table medicos alter COLUMN  ativo set not null;
--- src/main/resources/db/migration/V7__alter-table-pacientes-add-column-ativo.sql
+++ src/main/resources/db/migration/V7__alter-table-pacientes-add-column-ativo.sql
@@ -1,3 +1,3 @@
-alter table pacientes add column ativo smallint;
-update pacientes set ativo = 1;
+alter table pacientes add column ativo boolean default true;
+update pacientes set ativo = true;
 alter table pacientes alter COLUMN  ativo set not null;
